c12100fcc5552985484b5bd5d452e86894e1349e
Exedward <eduardo.ex7@gmail.com>
2023-03-02 19:13:36 -0300

    .

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as Notifications from 'expo-notifications'
+
+Notifications.setNotificationHandler({
+  handleNotification:  async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowAlert: true,
+  })
+})
 
 export default function App() {
   return (
