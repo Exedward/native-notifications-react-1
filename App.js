a505ec7ec7b344c920bfa36186f2fdf8721df01e
Exedward <eduardo.ex7@gmail.com>
2023-03-03 14:01:04 -0300

    .

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Button, Alert } from 'react-native';
 import * as Notifications from 'expo-notifications'
@@ -12,21 +13,38 @@ Notifications.setNotificationHandler({
 
 export default function App() {
 
-  async function handleCallNotification(){
+  const [tokenPush, setTokenPush] = useState<string>('')
+
+  useEffect(() => {
+    handleTokenPushNotification()
+  }, [])
+
+  const handleTokenPushNotification = async () => {
     const { status } = await Notifications.getPermissionsAsync()
     if(status != 'granted'){
       Alert.alert('Voce nao tem permissao para notifications.')
       return;
     }
     let token = (await Notifications.getExpoPushTokenAsync()).data
-    console.log(token)
+    setTokenPush(token)
+  }
 
-    await Notifications.scheduleNotificationAsync()
+  async function handleCallNotification(){
+    await Notifications.scheduleNotificationAsync({
+      content:{
+        title: 'Aviso',
+        body: 'Ó sistema está ok.',
+        data:{},
+      },
+      trigger:{
+        seconds: 4,
+      }
+    })
   }
 
   return (
     <View style={styles.container}>
-      <Text>Edu.</Text>
+      <Text>Token: {tokenPush}</Text>
       <Button title='Chamar Notifications' onPress={handleCallNotification}/>
       <StatusBar style="auto" />
     </View>
